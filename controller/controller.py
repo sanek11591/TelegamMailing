3a5e445d2a38215050c7b630efa346cf35a4e65b
Alex <alex29691@yandex.ru>
2023-09-10 12:05:45 +0300

    оптимизация контроллера

--- controller/controller.py
+++ controller/controller.py
@@ -12,28 +12,20 @@ class Controller:
         t = Thread(target=view.utils.lister_of_base)
         t.start()
 
-        @self.bot.message_handler(commands=['admin'])
-        async def send_message_to_model(message):
-            if utils.is_admin_check(message):
-                await Admin.admin_keyboard(self.bot, message.chat.id)
-            else:
-                await self.bot.send_message(message.chat.id, "Команда не доступна")
-
         @self.bot.message_handler(content_types=['text'])
         async def send_command_to_admin(message):
-            if message.text == 'Добавить рассылку' or message.chat.id in self.flag:
-                if utils.is_admin_check(message):
+            if utils.is_admin_check(message):
+                if message.text == 'Добавить рассылку' or message.chat.id in self.flag:
                     if message.chat.id not in self.flag:
                         self.flag.append(await Admin.add_mailing(bot, message.chat.id, message.text))
                     else:
                         if await Admin.add_mailing(bot, message.chat.id, message.text) == 0:
                             self.flag.remove(message.chat.id)
-                else:
-                    await self.bot.send_message(message.chat.id, "Команда не доступна")
-            if message.text == 'Удалить рассылку':
-                if utils.is_admin_check(message):
+                elif message.text == 'Удалить рассылку':
                     await self.bot.send_message(message.chat.id, 'Удаление будет позже')
+                elif message.text == '/admin':
+                    await Admin.admin_keyboard(self.bot, message.chat.id)
                 else:
-                    await self.bot.send_message(message.chat.id, "Команда не доступна")
+                    await self.bot.send_message(message.chat.id, "Команда не существует")
             else:
-                await self.bot.send_message(message.chat.id, "Команда не существует")
+                await self.bot.send_message(message.chat.id, "Команда не существует или не доступна")
