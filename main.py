a8eda11d5c93179b94bc5aa4279ed951532480a3
sanek11591 <alex29691@yandex.ru>
2023-09-24 20:45:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from controller import controller
 import asyncio
 from telebot.async_telebot import AsyncTeleBot
 
-bot = AsyncTeleBot('6562747970:AAGcckmVkQqbqkBmkAQT4Jw70xe23t9EEg4')
+bot = AsyncTeleBot('token')
 
 if __name__ == '__main__':
     controller.Controller(bot)
